5ab3b5da51b09cd4a870db834c6006073b6d62ea
SpiritAndPollen <jrtt212@gmail.com>
2025-07-20 15:13:54 +1000

    Initial Commit

---
+++ JRNL/JRNLApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRNLApp.swift
+//  JRNL
+//
+//  Created by Jedda Tuuta on 20/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct JRNLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
